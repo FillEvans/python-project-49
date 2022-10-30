2e0f9a919d80a38123c9d7b41283f63450c1e265
Fill Evans <filipp.glinchuk@gmail.com>
2022-10-30 12:37:05 +0300

    preparing...

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import prompt
+from brain_games.cli import welcome_user
+
 def welcome():
     print('Welcome to the Brain Games!')
 
 
 def main():
     welcome()
+    welcome_user()
 
 
 if __name__ == '__main__':
