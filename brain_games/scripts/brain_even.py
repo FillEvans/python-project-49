d14cd140621e541b2010d080c8adc693167ce66f
Fill Evans <filipp.glinchuk@gmail.com>
2022-11-03 01:27:03 +0300

    fixed game in brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
-import prompt
 from brain_games.cli import welcome_user
-from brain_games.scripts.brain_games import main
+import prompt
+from brain_games.scripts.brain_games import welcome
 
-main()
-# welcome_user()
+
+welcome()
 
 
 def is_even():
-    print('Answer "yes" if the number is even, otherwise answer "no".\nQuestion: 15')
+    name = welcome_user()[1]
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('Question: 15')
     answer1 = prompt.string('Your answer: ')
     if answer1 != 'no':
-        return print(f"{answer1} is wrong answer ;(. Correct answer was 'no'\nLet's try again, {name}")
+        print(f"'{answer1}' is wrong answer ;(. Correct answer was 'no'.")
+        return print(f"Let's try again, {name}!")
     print('Correct!\nQuestion: 6')
     answer2 = prompt.string('Your answer: ')
     if answer2 != 'yes':
-        return print(f"{answer2} is wrong answer ;(. Correct answer was 'yes'\nLet's try again, {name}")
+        print(f"'{answer2}' is wrong answer ;(. Correct answer was 'yes'.")
+        return print(f"Let's try again, {name}!")
     print('Correct!\nQuestion: 7')
     answer3 = prompt.string('Your answer: ')
     if answer3 != 'no':
-        return print(f"{answer3} is wrong answer ;(. Correct answer was 'no'\nLet's try again, {name}")
-    print(f'Correct!\nCongratulations, {name}')
+        print(f"'{answer3}' is wrong answer ;(. Correct answer was 'no'.")
+        return print(f"Let's try again, {name}!")
+    print(f'Correct!\nCongratulations, {name}!')
 
 
-is_even()
+def main():
+    is_even()
+
+
+if __name__ == '__main__':
+    main()
