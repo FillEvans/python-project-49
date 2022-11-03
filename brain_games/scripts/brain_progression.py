ca85462ce2ed1b03ca69a2bf94aee2f471d62cba
Fill Evans <filipp.glinchuk@gmail.com>
2022-11-03 20:19:59 +0300

    fixed each game, done

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
 import prompt
+import random
+from random import randint
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_games import welcome
 
 
 welcome()
+name = welcome_user()[1]
 
 
-def progression():
-    name = welcome_user()[1]
+def game_progression():
     print('What number is missing in the progression?')
-    print('Question: 5 7 9 11 13 .. 17 19 21 23')
-    answer_1 = prompt.integer('Your answer: ')
-    if answer_1 != 15:
-        print(f"'{answer_1}' is wrong answer ;(. Correct answer was '15'.")
-        return print(f"Let's try again, {name}!")
-    print('Correct!\nQuestion: 2 5 8 .. 14 17 20 23 26 29')
-    answer_2 = prompt.integer('Your answer: ')
-    if answer_2 != 11:
-        print(f"'{answer_2}' is wrong answer ;(. Correct answer was '11'.")
-        return print(f"Let's try again, {name}!")
-    print('Correct!\nQuestion: 14 19 24 29 34 39 44 49 54 ..')
-    answer_3 = prompt.integer('Your answer: ')
-    if answer_3 != 59:
-        print(f"'{answer_3}' is wrong answer ;(. Correct answer was '59'.")
-        return print(f"Let's try again, {name}!")
-    print(f"Correct!\nCongratulations, {name}!")
+    good_result = 0
+    while good_result <= 3:
+        stop = randint(20, 50)
+        step = randint(1, 5)
+        result = list(range(randint(0, 9), stop, step))[0: 5]
+        answer = str(result[randint(0, 4)])
+        result = " ".join(map(str, (result)))
+        printer = result.replace(answer, "..", 1)
+        print(f"Question: {printer}")
+        answer_1 = prompt.integer('Your answer: ')
+        if int(answer) != answer_1:
+            return print(
+                f"'{answer_1}' is wrong answer ;(. Correct answer was '{answer}'."
+                f"\nLet's try again, {name}!")
+        else: 
+            print('Correct!')
+            good_result += 1
+            if good_result == 3:
+                return print(f"Congratulations, {name}!")
 
 
 def main():
-    progression()
+    game_progression()
 
 
 if __name__ == '__main__':
