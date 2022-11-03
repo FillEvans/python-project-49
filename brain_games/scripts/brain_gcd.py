0bf856c1a41574e38f3162ef8f355f7646331a70
Fill Evans <filipp.glinchuk@gmail.com>
2022-11-03 18:15:23 +0300

    fixed some games

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 import prompt
+import math
+import random
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_games import welcome
 
 
 welcome()
+name = welcome_user()[1]
 
 
-def gcd():
-    name = welcome_user()[1]
+def game_gcd():
     print('Find the greatest common divisor of given numbers.')
-    print('Question: 25 50')
-    answer_1 = prompt.integer('Your answer: ')
-    if answer_1 != 25:
-        print(f"'{answer_1}' is wrong answer ;(. Correct answer was '25'.")
-        return print(f"Let's try again, {name}!")
-    print('Correct!\nQuestion: 100 52')
-    answer_2 = prompt.integer('Your answer: ')
-    if answer_2 != 4:
-        print(f"'{answer_2}' is wrong answer ;(. Correct answer was '4'.")
-        return print(f"Let's try again, {name}!")
-    print('Correct!\nQuestion: 3 9')
-    answer_3 = prompt.integer('Your answer: ')
-    if answer_3 != 3:
-        print(f"'{answer_3}' is wrong answer ;(. Correct answer was '3'.")
-        return print(f"Let's try again, {name}!")
-    print(f"Correct!\nCongratulations, {name}!")
+    good_result = 0
+    while good_result <= 3:
+        num_1 = random.randint(1, 50)
+        num_2 = random.randint(1, 50)
+        print(f"Question: {num_1} {num_2}")
+        answer = prompt.integer('Your answer: ')
+        result = math.gcd(num_1, num_2)
+        if answer != result:
+            return print(
+                f"'{answer}' is wrong answer ;(. Correct answer was '{result}'"
+                f"\nLet's try again, {name}!")
+        good_result += 1
+        print('Correct!')
+        if good_result == 3:
+            return print(f"Congratulations, {name}!")
 
 
 def main():
-    gcd()
+    game_gcd()
 
 
 if __name__ == '__main__':
